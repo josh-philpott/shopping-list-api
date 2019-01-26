08ec985f415bc5e565ebc400c92b3cb27366bf3b
Josh Philpott <jphil529@gmail.com>
2019-01-26 08:16:10 -0700

    feat: add post and get shopping lists

--- routes/shopping-lists.js
+++ routes/shopping-lists.js
@@ -2,9 +2,8 @@ var express = require("express")
 var router = express.Router()
 var uuidv1 = require("uuid/v1")
 
-/* GET users listing. */
-router.get("/:id", function(req, res, next) {
-  res.send([
+const shoppingLists = {
+  "shopping-list-one": [
     {
       id: uuidv1(),
       category: "Produce",
@@ -50,7 +49,34 @@ router.get("/:id", function(req, res, next) {
       quantityType: "cups",
       inCart: false
     }
-  ])
+  ]
+}
+
+/* GET users listing. */
+router.get("/:id", function(req, res, next) {
+  res.send(shoppingLists[req.params.id])
 })
 
+/* POST with id */
+router.post("/:id", function(req, res, next) {
+  //TODO: Validation
+
+  if (req.body[req.params.id]) {
+    shoppingLists[req.params.id] = req.body[req.params.id]
+  } else {
+    shoppingLists[req.params.id] = []
+  }
+  res.send(shoppingLists[req.params.id])
+})
+
+/* POST with no id creates a new list and returns the id... If it contains a list, saves the list */
+router.post("/", function(req, res, next) {
+  const key = uuidv1()
+  if (req.body.shoppingList) {
+    shoppingLists[key] = req.body.shoppingList
+  } else {
+    shoppingLists[key] = []
+  }
+  res.send({ id: key, shoppingList: shoppingLists[key] })
+})
 module.exports = router
